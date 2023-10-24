eb7a0dd2125dee8000b4bbfc9c73f0ac83cd30bf
Qianming <e-Ming.Qian@geely.com>
2023-10-24 10:42:32 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from "vue"
+const canvas = ref(undefined)
+</script>
+<template>
+  <div>
+    <canvas ref="canvas"></canvas>
+  </div>
+</template>
+
+<style scoped></style>
